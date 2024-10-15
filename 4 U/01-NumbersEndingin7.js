f6028e412bed803cafbb9d3b0258c499b3a3b436
YavorStavrev <156719018+YavorStavrev@users.noreply.github.com>
2024-10-15 21:34:33 +0300

    Delete 4 U directory

--- 4 U/01-NumbersEndingin7.js
+++
@@ -1,8 +0,0 @@
-function NumberEnding(){
-    for(i = 0; i <= 1000; i++){
-        if(i % 10 === 7){
-            console.log(i);
-        }
-    }
-}
-NumberEnding(['']);
--- 4 U/02-MultipricationTable.js
+++
@@ -1,9 +0,0 @@
-function MultiTable(input){
-    let num = Number(input[0]);
-    sum = 0;
-    for(i = 1; i <=10; i++){
-        sum = i * num;
-        console.log(`${i} * ${num} = ${sum}`);
-    }
-}
-MultiTable(['5']);
